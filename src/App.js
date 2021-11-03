3d0f047cae2e03375ec983a17d86881c041158c1
Godspower Eze <Godspowereze260@gmail.com>
2021-11-04 00:35:11 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const Web3 = require('web3')
 
 function App() {
+
+  let [connected, setConnected] = useState(false);
+  let [account, setAccount] = useState("Ox")
+
+  useEffect(() => {
+    let run = async () => {
+      checkConnection();
+    }
+    run();
+  },[connected])
+
+  const checkConnection = async () => {
+    if(window.ethereum){
+      let accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      if (accounts.length > 0){
+        setConnected(true)
+        setAccount(accounts[0])
+      }
+    }
+  }
+
+  const connect = async (remoteClient) => {
+    try {
+        if (remoteClient = 1){
+            if (window.ethereum){
+                await window.ethereum.send('eth_requestAccounts');
+                window.web3 = new Web3(window.ethereum);
+                let accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+                setAccount(accounts[0])
+                setConnected(() => true)
+            }else{
+              return alert("Please Install Metamask");
+            }
+        }
+    }catch(err){
+        console.error(err)
+    }
+  
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {connected ? <span>Connected with <b>{account}</b></span> :<button onClick={() => connect(1)}>Connect to Metamask</button>}
       </header>
     </div>
   );
