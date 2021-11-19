4909baf36b224183d3cc5b7e79940ca051fd5421
Godspower Eze <Godspowereze260@gmail.com>
2021-11-19 06:47:37 +0100

    Fixed walletconnect error

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,64 +1,81 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useWeb3React } from '@web3-react/core';
 import { injected, walletconnect, authereum, walletlink, portis } from "../connectors";
 import { getChainName } from '../utils';
+import { NoEthereumProviderError, UserRejectedRequestError as UserRejectedRequestErrorInjected } from '@web3-react/injected-connector';
+import { UserRejectedRequestError as UserRejectedRequestErrorWalletConnect } from '@web3-react/walletconnect-connector';
+import { UnsupportedChainIdError } from '@web3-react/core';
 
 const Wallet = () => {
     let [loading, setLoading] = useState(false)
+    let [errorRenderer, setErrorRenderer] = useState(false)
     const {
-        account,
-        activate,
-        active,
-        chainId,
-        connector,
-        deactivate,
-        error,
-        provider,
-        setError,
-    } = useWeb3React();
+      account,
+      activate,
+      active,
+      chainId,
+      connector,
+      deactivate,
+      error,
+      provider,
+      setError,
+  } = useWeb3React();
+
+    useEffect(()=>{
+      errorHandler(error)
+    },[error])
+
+    const errorHandler = (error) => {
+      if(error !== undefined){
+        if(error instanceof UserRejectedRequestErrorWalletConnect){
+          console.log("User Rejected")
+          window.location.reload()
+        }else if(error instanceof UserRejectedRequestErrorInjected){
+          console.log("User Rejected Injected")
+        }else if(error instanceof UnsupportedChainIdError){
+          let chain = getChainName(chainId)
+          window.alert("Unsupported Chain. Connect to a supported chain")
+        }
+      }
+    }
     
     async function connectWithMetaMask() {
       try {
         await activate(injected)
+      } catch (error) {
         console.log(error)
-      } catch (ex) {
-        console.log(ex)
       }
     }
 
     async function connectWithWalletConnect() {
       try {
         await activate(walletconnect)
+      } catch (error) {
         console.log(error)
-      } catch (ex) {
-        console.log(ex)
       }
     }
 
     async function connectWithPortis() {
       try {
         await activate(portis)
+      } catch (error) {
         console.log(error)
-      } catch (ex) {
-        console.log(ex)
       }
     }
 
     async function connectWithWalletLink() {
       try {
         await activate(walletlink)
+      } catch (error) {
         console.log(error)
-      } catch (ex) {
-        console.log(ex)
       }
     }
 
     async function connectWithAuthereum() {
       try {
         await activate(authereum)
+      } catch (error) {
         console.log(error)
-      } catch (ex) {
-        console.log(ex)
       }
     }
 
